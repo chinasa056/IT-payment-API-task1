3fad9d2ca84a58f948167aad6159bc4b0bc9d5b0
chinasa056 <chinasaacha05@gmail.com>
2025-05-22 16:39:47 +0100

    feat: configured swagger ui express for documentation

--- server.js
+++ server.js
@@ -23,6 +23,59 @@ redis.on("connect", () => console.log("connection to redis successful")
 redis.on('error', () => console.log('Unable to connect to redis')
 );
 
+app.use((error, req, res, next) => {
+  if(error){
+     return res.status(400).json({message:  error.message})
+  }
+  next()
+})
+
+const swaggerJsdoc = require("swagger-jsdoc");
+const swagger_UI = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: "Payment Integration Task",
+      version: '1.0.0',
+      description: "Documentation for InternPulse Task, a platform that allows small business owners accept payment using paystack payment gateway",
+      license: {
+        name: 'BASE_URL:https://hubspot-k95r.onrender.com',
+      },
+      contact: {
+        name: "Chinasa Acha",
+         url: 'https://www.linkedin.com/in/chinasa-acha'
+      }
+    },
+    components: {
+      securitySchemes: {
+        BearerAuth: {
+          type: "http",
+          scheme: "bearer",
+           bearerFormat: "JWT"
+        }
+      }
+    }, 
+    security: [{ BearerAuth: [] }],
+    servers: [
+      {
+        url: "https://hubspot-k95r.onrender.com",
+        description: "Production Server"
+      },
+      {
+        url: "http://localhost:1112",
+        description: "Development Server"
+      }
+    ],
+    
+  },
+  apis: ["./routes/*.js"]
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+app.use("/paymentdoc", swagger_UI.serve, swagger_UI.setup(openapiSpecification))
+
 
 
 app.listen(PORT, () => {
