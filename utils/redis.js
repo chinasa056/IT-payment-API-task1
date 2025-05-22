030b57726b3e20dbd159a053ef799a5a5e9a4761
chinasa056 <chinasaacha05@gmail.com>
2025-05-22 17:01:01 +0100

    chore: uninstalled ioredis and removed instances of local redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,8 @@
-const Redis = require("ioredis");
+// const Redis = require("ioredis");
 
-const redis = new Redis({
-    host: "localhost",
-    port: 6379
-});
+// const redis = new Redis({
+//     host: "localhost",
+//     port: 6379
+// });
 
-module.exports = redis
+// module.exports = redis
